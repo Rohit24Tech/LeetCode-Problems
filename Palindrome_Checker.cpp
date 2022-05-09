e0fb8c5a5e0457b6d950c7621c8614e94cca4ffe
Rohit Adak <69696532+Rohit24Tech@users.noreply.github.com>
2022-05-09 22:34:19 +0530

    Palindrome_Checker.cpp

---
+++ Palindrome_Checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str=to_string(x);
+        string rev=str;
+        reverse(rev.begin(),rev.end());
+        if(str==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};

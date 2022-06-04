b3b90bf955408435b5471ca84d024885a61270a7
Rohit Adak <69696532+Rohit24Tech@users.noreply.github.com>
2022-06-05 03:28:44 +0530

    Update String_Palindrome_checker.cpp

--- String_Palindrome_checker.cpp
+++ String_Palindrome_checker.cpp
@@ -1,3 +1,5 @@
+//Recursive Solution: Takes a lot of time
+
 bool helper(int l, int r, string &s){
     if(l>=r)
         return true;
@@ -9,3 +11,23 @@ bool isPalindrome(string &s)
 {
 	return helper(0,s.size()-1,s);
 }
+
+//Linear Solution: Takes relatively less time
+
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    for(int i=0;i<(S.length())/2;i++){
+	        if(S[i]!=S[S.length()-1-i]){
+	            return false;
+	            break;
+	        }
+	    }
+	    return true;
+	}
+
+};
